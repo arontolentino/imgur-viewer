214ffd9c2fe76a6bfbf52a2c5fbf13b3d433eed0
Aron Tolentino <arontolentino@outlook.com>
2019-08-23 21:36:55 -0400

    Create ImageList and UploadForm components/routes

--- src/main.js
+++ src/main.js
@@ -3,12 +3,22 @@ import VueRouter from 'vue-router';
 import App from './App';
 import store from './store/index';
 import AuthHandler from './components/AuthHandler';
+import ImageList from './components/ImageList.vue';
+import UploadForm from './components/UploadForm.vue';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      component: ImageList
+    },
+    {
+      path: '/upload',
+      component: UploadForm
+    },
     {
       path: '/oauth2/callback',
       component: AuthHandler
