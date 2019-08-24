325fcbd11a376db976011e8bea5a6b2fe60bca1b
Aron Tolentino <arontolentino@outlook.com>
2019-08-23 21:26:46 -0400

    Redirect user programmatically after logging in

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,5 +1,6 @@
 import api from '../../api/imgur';
 import qs from 'qs';
+import { router } from '../../main';
 
 const state = {
   token: window.localStorage.getItem('imgur_token')
@@ -19,6 +20,7 @@ const actions = {
 
     commit('setToken', query.access_token);
     window.localStorage.setItem('imgur_token', query.access_token);
+    router.push('/');
   },
 
   logout: ({ commit }) => {
